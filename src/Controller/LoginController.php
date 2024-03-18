bfd3d5a8b2d18c34d0b9dd8cc0e84d6c3eae09c4
Quentout187 <quentin.caron@ecoles-epsi.net>
2024-03-18 15:58:43 +0100

    wololo

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class LoginRegisterController extends AbstractController
+class LoginController extends AbstractController
 {
-    #[Route('/login/register', name: 'app_login_register')]
+    #[Route('/login', name: 'app_login')]
     public function index(): Response
     {
-        return $this->render('login_register/index.html.twig', [
-            'controller_name' => 'LoginRegisterController',
+        return $this->render('login/index.html.twig', [
+            'controller_name' => 'LoginController',
         ]);
     }
 }
